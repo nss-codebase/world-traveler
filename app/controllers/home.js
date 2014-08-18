918f27f0158d04c6760418fdb177612257ef28ee
Chyld Medford <chyld.medford@gmail.com>
2014-08-18 09:42:08 -0500

    basic files

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,3 +4,15 @@ exports.index = function(req, res){
   res.render('home/index');
 };
 
+exports.contact = function(req, res){
+  res.render('home/contact');
+};
+
+exports.about = function(req, res){
+  res.render('home/about');
+};
+
+exports.faq = function(req, res){
+  res.render('home/faq');
+};
+
